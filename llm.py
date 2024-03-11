b45b5f54ef4117b7c400cff9b57f2c6a7ea09637
Joseph <joemenjr@gmail.com>
2024-03-10 20:57:17 -0400

    larger prompt

--- llm.py
+++ llm.py
@@ -13,7 +13,10 @@ response = completion(
                          Additional data is incoming. The maximum amount and minimum amount to finance for each lender varies.  Wells Fargo accepts a minimum of $10,000 and a maximum of
                          $100,000.  Ally accepts a minimum of $5,000 and a maximum of $50,000. Santandar accepts a minimum of $2,000 and a maximum of $30,000.  It is important that the vehicle
                          price is within range.  The user would like an SUV which starts at $40,000 a car at $10,000 and a Truck at $90,000.  What do you think is an applicable lender now?
-                          ""","role": "user"}], 
+                         The car selection of the applicant is influential.  Given that the applicant prefers an SUV what would be the most appropriate lender?
+                         
+                         We also have another peice of data.  The applicant and the co-signer do not have Identificaiton.  Ally is not willing to work with applicants with no ID, Sandtandar likewise
+                         however Wells Fargo will allow for a applicants with no ID.  What would be the most appropriate choice?""","role": "user"}], 
             api_base="http://localhost:11434"
 )
 
