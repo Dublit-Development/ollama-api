6abb36d01c53cb009e289f577d7d4a044ff44d40
Joseph <joemenjr@gmail.com>
2024-03-10 20:25:47 -0400

    larger prompt

--- llm.py
+++ llm.py
@@ -2,7 +2,10 @@ from litellm import completion
 
 response = completion(
             model="ollama/llama2", 
-            messages = [{ "content": "Hello,  How are you?","role": "user"}], 
+            messages = [{ "content": """Hello,  How are you?.  You are going to be a financial analyst bot for a Automotive Company.  You need to make a selection of the most
+                         appropriate lender given a applicant and his/her credit score and other details.  In the first example we will have a applicant with a credit score of 600 and three
+                         lenders to choose from.  Wells Fargo accepts a credit score of 500,  Ally accepts a credit score of 600 and Santandar accepts a credit score of 800. 
+                         What is the most appropriate lender?""","role": "user"}], 
             api_base="http://localhost:11434"
 )
 
