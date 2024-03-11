de0353c36e98eaaa999f5911e15114303e20dbcf
Joseph <joemenjr@gmail.com>
2024-03-10 20:30:56 -0400

    larger prompt

--- llm.py
+++ llm.py
@@ -5,7 +5,8 @@ response = completion(
             messages = [{ "content": """Hello,  How are you?.  You are going to be a financial analyst bot for a Automotive Company.  You need to make a selection of the most
                          appropriate lender given a applicant and his/her credit score and other details.  In the first example we will have a applicant with a credit score of 600 and three
                          lenders to choose from.  Wells Fargo accepts a credit score of 500,  Ally accepts a credit score of 600 and Santandar accepts a credit score of 800. 
-                         What is the most appropriate lender?""","role": "user"}], 
+                         What is the most appropriate lender? Let us add some additional parameters.  The applicant also has a co-signer.  The co-signer has a score of 800 given that
+                         that the applicant has a co-signer that will take priortiy credit score.  What is the most appropriate lender now?""","role": "user"}], 
             api_base="http://localhost:11434"
 )
 
